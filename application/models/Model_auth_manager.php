439db280c50b1e03872458b1ddeff724a5cebc37
Muhammad Irfan <funnyirfan321@gmail.com>
2018-07-16 21:28:16 +0700

    Update
    Signed-off-by: Muhammad Irfan <funnyirfan321@gmail.com>

---
+++ application/models/Model_auth_manager.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_auth_manager extends CI_Model
+{
+    public function get()
+    {
+
+    }
+}
---
+++ application/models/Model_auth_operator.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_auth_operator extends CI_Model
+{
+    public function get()
+    {
+
+    }
+}
